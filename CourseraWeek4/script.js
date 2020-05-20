90ff38340623761870f4c77dd78a8484a9ce5be8
vedantharshu <vedantharbu@gmail.com>
2020-05-20 16:37:37 +0530

    week5 coursera

--- CourseraWeek4/script.js
+++ CourseraWeek4/script.js
@@ -31,7 +31,6 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -62,5 +61,3 @@ for (var i = 0; i < names.length; i++) {
     helloSpeaker.speak(names[i]);
   }
 }
-
-})();
